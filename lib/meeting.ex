07d56a303f3c54c7ede8e6d406f0b06e3ab2c755
otherchris <chriscaragianis@gmail.com>
2020-04-20 20:02:31 -0400

    changed meeting

--- lib/meeting.ex
+++ lib/meeting.ex
@@ -11,16 +11,18 @@ defmodule Meeting do
   defstruct [
     :chair,
     :speaker,
-    :vote,
-    :last_vote,
-    motion_stack: [],
+    :current_vote,
+    :all_votes,
+    :question,
+    question_stack: [],
+
     waiting_for_second: false,
     voting: false
   ]
 
   @type vote() :: %{member_id: String.t(), vote: String.t()}
   @type vote_result() :: %{question: question(), votes: list(vote())}
-  @type question() :: %{content: String.t, type: Atom.t()}
+  @type question() :: %{content: String.t(), type: Atom.t(), subject: String.t(), object: any()}
 
   @type t() :: %Meeting{
     chair: String.t(),
@@ -43,18 +45,19 @@ defmodule Meeting do
   @doc """
   Make a motion 
   """
-  @spec motion(Actions.data()) :: Meeting.t()
-  def motion_to_adjourn({meeting, _, _}) do
+  @spec motion(Meeting.t(), question()) :: Meeting.t()
+  def motion({meeting, motion}) do
     meeting
-    |> Map.put(:motion_stack, meeting.motion_stack ++ [:motion_to_adjourn])
-    |> Map.put(:waiting_for_second, true)
+    |> Map.put(:question_stack, [question] ++ meeting.question_stack)
+    |> Map.put(:waiting_for_second, true
+    )
   end
 
   @doc """
   Seconds the motion on the floor 
   """
-  @spec second(Actions.data()) :: Meeting.t()
-  def second({meeting, _, _}), do: Map.put(meeting, :waiting_for_second, false)
+  @spec second(Meeting.t()) :: Meeting.t()
+  def second(meeting), do: Map.put(meeting, :waiting_for_second, false)
 
   @doc """
   Calls a vote
@@ -62,32 +65,31 @@ defmodule Meeting do
   @spec call_vote(Actions.data()) :: Meeting.t()
   def call_vote({meeting, _, _}) do
     meeting
-    |> Map.put(:vote, %{yeas: [], nays: []})
+    |> Map.put(:current_vote, [])
     |> Map.put(:voting, true)
   end
 
   @doc """
   Applies a vote
   """
-  @spec vote(Actions.data()) :: Meeting.t()
-  def vote({meeting = %{vote: %{yeas: yeas, nays: nays}}, subject, choice}) do
-    [yeas, nays] = Enum.map([yeas, nays], &Enum.reject(&1, fn(x) -> x == subject end))
-    [yeas, nays] =
-      case choice do
-        :yea -> [yeas ++ [subject], nays]
-        :nay -> [yeas, nays ++ [subject]]
-      end
-    Map.put(meeting, :vote, %{yeas: yeas, nays: nays})
+  @spec vote(Meeting.t(), String.t(), String.t()) :: Meeting.t()
+  def vote(meeting, subject_id, vote) do
+    new_vote =
+      meeting.current_vote
+      |> Enum.reject(fn(v) => v.member_id == subject_id end)
+      |> Kernel.++(%{member_id: subject_id, vote: vote})
+    Map.put(meeting, :current_vote, new_vote)
   end
 
   @doc """
   Concludes and saves a vote
   """
-  @spec end_vote(Actions.data()) :: Meeting.t()
-  def end_vote({meeting = %{vote: vote}, _, _}) do
+  @spec end_vote(Meeting.t()) :: Meeting.t()
+  def end_vote(meeting = %{question_stack: [curr | rest], all_votes: all_votes, vote: vote}) do
     meeting
-    |> Map.put(:last_vote, vote)
-    |> Map.put(:vote, %{})
+    |> Map.put(:all_votes, all_votes ++ [%{vote: vote, question: hd(meeting.question_stack))
+    |> Map.put(:question_stack, rest)
+    |> Map.put(:current_vote, [])
     |> Map.put(:voting, false)
   end
 end
